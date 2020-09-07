888076f45925309e95442825c6d31208bf417cb7
ishvar99 <ishanvarshney99@gmail.com>
2020-09-07 13:27:42 +0530

    setup server using json-server and displayed logs

---
+++ src/components/logs/Logs.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react"
+import LogItem from "./LogItem"
+export const Logs = () => {
+  const [logs, setLogs] = useState([])
+  const [loading, setLoading] = useState()
+  useEffect(() => {
+    getLogs()
+  }, [])
+  const getLogs = async () => {
+    setLoading(true)
+    const res = await fetch("/logs")
+    const data = await res.json()
+    setLogs(data)
+    setLoading(false)
+  }
+  return (
+    <div>
+      {loading ? (
+        <h4>loading...</h4>
+      ) : (
+        <ul className="collection with-header">
+          <li className="collection-header">
+            <h4 className="center">System Logs</h4>
+          </li>
+          {logs.length === 0 ? (
+            <p className="center">No Logs to display...</p>
+          ) : (
+            logs.map((log) => <LogItem log={log} key={log.id} />)
+          )}
+        </ul>
+      )}
+    </div>
+  )
+}
